47e58534dbd82c95d38a0d61b2992c6276ad027b
Jakub Chodziński <xapixowy@gmail.com>
2025-04-04 03:33:42 +0200

    delete i18next debug mode and force language

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,12 +3,9 @@ import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 import { i18nConfig } from './configs/i18n';
-import { Language } from './enums/language.enum';
 
 i18n.use(initReactI18next).use(Backend).use(LanguageDetector).use(initReactI18next).init({
-  lng: Language.en,
   fallbackLng: i18nConfig.fallbackLanguage,
-  debug: true,
 });
 
 export default i18n;
