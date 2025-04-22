b83b3bea2835d8a11b9ab5f042dda11fc02e5cc4
Jakub Chodziński <xapixowy@gmail.com>
2025-04-22 14:10:50 +0200

    changes in auth-layout

--- src/layouts/auth-layout/auth-layout.tsx
+++ src/layouts/auth-layout/auth-layout.tsx
@@ -1,4 +1,4 @@
-import heroBackgroundImage600 from '@/assets/landing-page/hero-background-600.webp';
+import backgroundImage600 from '@/assets/auth-layout/background-600.webp';
 import BrandLink from '@/components/global/brand-link';
 import MaxWidthWrapper from '@/components/layout/max-width-wrapper';
 import { ReactNode } from 'react';
@@ -16,22 +16,22 @@ const AuthLayout = (props: AuthLayoutProps) => {
   const { t } = useTranslation();
 
   return (
-    <div className={`grid min-h-screen w-full place-items-center ${props.className ?? ''}`}>
+    <div className={`grid min-h-screen w-full place-items-center overflow-hidden ${props.className ?? ''}`}>
       <MaxWidthWrapper className='p-8'>
         <header className='grid place-items-center gap-2'>
           <BrandLink />
-          {props.title && <h1 className='heading-6 font-normal'>{props.title}</h1>}
+          {props.title && <h1 className='heading-6 text-center font-normal'>{props.title}</h1>}
         </header>
-        <main className='my-12'>
-          {props.children}
+        <main className='before:from-my-accent-700/70 before:to-my-secondary-700/70 before:via-my-primary-700/30 relative my-12 min-h-108 w-96 before:absolute before:inset-[-1px] before:-z-1 before:rounded-3xl before:bg-gradient-to-tr'>
+          <div className='min-h-[inherit] rounded-3xl bg-zinc-950/90 p-8'>{props.children}</div>
           <LazyLoadImage
             visibleByDefault={true}
-            className='absolute top-1/2 left-1/2 -z-1 max-w-none -translate-1/2'
+            className='animate-show-up absolute top-1/2 left-1/2 -z-1 w-[1000px] max-w-none -translate-1/2'
             alt={t('LandingPage.Abstract background with color blur')}
-            src={heroBackgroundImage600}
+            src={backgroundImage600}
           />
         </main>
-        <footer className='flex flex-col'>{props.footerChildren}</footer>
+        <footer className='flex flex-col text-center'>{props.footerChildren}</footer>
       </MaxWidthWrapper>
     </div>
   );
